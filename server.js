ad9adcb77b650a68ab2805ab0ea7ae1c6ecd01e4
Mhsalameh <mohsalameh1@gmail.com>
2022-02-03 14:49:11 +0200

    changed pg client

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const dotEnv = require('dotenv');
 dotEnv.config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
-const client = new pg.Client(DATABASE_URL);
+// const client = new pg.Client(DATABASE_URL);
+
+const client = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 
 const PORT = process.env.PORT;
 const APIKEY = process.env.APIKEY;
